b922ee8e5a5c83b3bab4986cd41049572a03e66f
jonasach <jonasach@gmail.com>
2023-08-04 07:15:35 -0400

    [2023-08-04 07:15:32]

---
+++ git.sh
@@ -0,0 +1,13 @@
+
+# Get current timestamp
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Prompt for commit message
+read -p "Enter commit message: " message
+
+# Add and commit with timestamp
+git add .
+git commit -m "[$timestamp] $message"
+
+
+git push origin main
