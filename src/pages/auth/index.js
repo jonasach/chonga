c4deec048101c6f4b3bc0847d33cb00b8ba29602
jonasach <jonasach@gmail.com>
2023-08-06 06:59:35 -0400

    [2023-08-06 06:59:32] V13

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,6 +1,6 @@
 
 import {Layout} from 'src/layouts/auth/layout';
-import LoginForm from 'src/components/LoginForm';
+import LoginForm from 'src/components/loginform';
 
 export default function Login() {
   return (
